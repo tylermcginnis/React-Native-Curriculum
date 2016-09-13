51084cc8df3a026513f3791c53eee5a9ae0b0ff8
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-12 23:27:47 -0600

    Add DrawerHeader

--- app/components/FooterTabs/DrawerHeader.js
+++ app/components/FooterTabs/DrawerHeader.js
@@ -1,16 +1,51 @@
-import React, { PropTypes, Component } from 'react'
-import { View, Text } from 'react-native'
+import React, { PropTypes } from 'react'
+import { View, StyleSheet, Text, Image } from 'react-native'
+import { connect } from 'react-redux'
+import { colors, fontSizes } from '~/styles'
 
-export default class DrawerHeader extends Component {
-  static propTypes = {}
-  state = {}
-  render () {
-    return (
-      <View>
-        <Text>
-          DrawerHeader
-        </Text>
-      </View>
-    )
+DrawerHeader.propTypes = {
+  photoURL: PropTypes.string.isRequired,
+  displayName: PropTypes.string.isRequired,
+}
+
+function DrawerHeader (props) {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: props.photoURL}}/>
+      <Text style={styles.nameText}>{props.displayName}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 150,
+    alignItems: 'center',
+    paddingTop: 20,
+    borderBottomWidth: 1,
+    borderColor: colors.border,
+  },
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+  },
+  nameText: {
+    fontSize: fontSizes.primary,
+    color: colors.primary,
+    marginTop: 8,
+    marginBottom: 3,
+  },
+})
+
+function mapStateToProps ({users, authentication}) {
+  const { authedId } = authentication
+  return {
+    photoURL: authentication.photoURL,
+    displayName: authentication.displayName,
   }
 }
+
+export default connect(
+  mapStateToProps
+)(DrawerHeader)

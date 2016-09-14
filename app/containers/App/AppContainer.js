c23233c93a76d7bfca3994544f06da4f8f310b3b
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-14 09:54:26 -0600

    Changes for AddRedux

--- app/containers/App/AppContainer.js
+++ app/containers/App/AppContainer.js
@@ -2,18 +2,31 @@ import React, { PropTypes, Component } from 'react'
 import { View } from 'react-native'
 import { PollsNavigator } from '~/containers'
 import { PreSplash } from '~/components'
+import { connect } from 'react-redux'
 
-export default class AppContainer extends Component {
+class AppContainer extends Component {
   static propTypes = {
     isAuthenticating: PropTypes.bool.isRequired,
+    isAuthed: PropTypes.bool.isRequired,
   }
   render () {
     return (
       <View style={{flex: 1}}>
         {this.props.isAuthenticating === true
           ? <PreSplash />
-          : <PollsNavigator isAuthed={false} />}
+          : <PollsNavigator isAuthed={this.props.isAuthed} />}
       </View>
     )
   }
 }
+
+function mapStateToProps ({authentication}) {
+  return {
+    isAuthenticating: authentication.isAuthenticating,
+    isAuthed: authentication.isAuthed,
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(AppContainer)

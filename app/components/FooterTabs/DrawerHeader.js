79d8b823dda944a03a3a8a1d8005b3e53393f8d9
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-29 22:22:06 -0600

    Changes for DrawerHeader

--- app/components/FooterTabs/DrawerHeader.js
+++ app/components/FooterTabs/DrawerHeader.js
@@ -1,20 +1,46 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { connect } from 'react-redux'
+import { View, StyleSheet, Text, Image } from 'react-native'
+import { colors, fontSizes } from '~/styles'
 
 DrawerHeader.propTypes = {
-
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
 }
 
-export default function DrawerHeader (props) {
+function DrawerHeader (props) {
   return (
-    <View>
-      <Text>
-        DrawerHeader
-      </Text>
+    <View style={styles.container}>
+      <Image style={styles.image} source={{uri: props.avatar}}/>
+      <Text style={styles.nameText}>{props.name}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    height: 150,
+    alignItems: 'center',
+    paddingTop: 20,
+    borderBottomWidth: 1,
+    borderColor: colors.border,
+  },
+  image: {
+    height: 60,
+    width: 60,
+    borderRadius: 30,
+  },
+  nameText: {
+    fontSize: fontSizes.primary,
+    color: colors.primary,
+    marginTop: 8,
+    marginBottom: 3,
+  },
 })
+
+export default connect(
+  ({users, authentication}) => ({
+    avatar: users[authentication.authedId].info.avatar,
+    name: users[authentication.authedId].info.name,
+  })
+)(DrawerHeader)

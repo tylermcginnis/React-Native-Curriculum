252d7b84115f8561060907f485918ddc69622b92
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-29 21:41:56 -0600

    Changes for HomeView

--- app/components/Home/Home.js
+++ app/components/Home/Home.js
@@ -1,22 +1,29 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text, Platform } from 'react-native'
+import { View, StyleSheet, Text, Platform, ActivityIndicator, ListView } from 'react-native'
 import { PollsNavbar, NewPollIcon, HamburgerIcon } from '~/components'
+import { colors, fontSizes } from '~/styles'
 
 Home.propTypes = {
+  noPolls: PropTypes.bool.isRequired,
+  listenerSet: PropTypes.bool.isRequired,
+  dataSource: PropTypes.object.isRequired,
+  renderRow: PropTypes.func.isRequired,
   openDrawer: PropTypes.func,
   onNewPoll: PropTypes.func.isRequired,
 }
 
 export default function Home (props) {
   return (
-    <View styl>
+    <View style={styles.container}>
       <PollsNavbar
         title='All Polls'
         leftButton={Platform.OS === 'android' ? <HamburgerIcon onPress={props.openDrawer} /> : null}
         rightButton={<NewPollIcon onPress={props.onNewPoll}/>} />
-      <Text>
-        Home
-      </Text>
+      {props.listenerSet === false
+        ? <ActivityIndicator size='small' style={styles.activityIndicator} color={colors.secondary}/>
+        : props.noPolls === true
+            ? <Text style={styles.noPolls}>There are no polls 😞</Text>
+            : <ListView renderRow={props.renderRow} dataSource={props.dataSource} />}
     </View>
   )
 }
@@ -26,4 +33,13 @@ const styles = StyleSheet.create({
     flex: 1,
     marginBottom: 50,
   },
+  activityIndicator: {
+    marginTop: 30,
+  },
+  noPolls: {
+    textAlign: 'center',
+    color: colors.primary,
+    fontSize: fontSizes.primary,
+    margin: 30,
+  }
 })
--- app/containers/Home/HomeContainer.js
+++ app/containers/Home/HomeContainer.js
@@ -1,21 +1,74 @@
 import React, { PropTypes, Component } from 'react'
-import { Home } from '~/components'
+import { ListView } from 'react-native'
+import { connect } from 'react-redux'
+import { Home, PollPreview } from '~/components'
 
-export default class HomeContainer extends Component {
+class HomeContainer extends Component {
   static propTypes = {
+    polls: PropTypes.object.isRequired,
+    pollsVotedOn: PropTypes.object.isRequired,
+    ownPolls: PropTypes.object.isRequired,
+    listenerSet: PropTypes.bool.isRequired,
     openDrawer: PropTypes.func,
     navigator: PropTypes.object.isRequired,
+  }
+  constructor (props) {
+    super(props)
+    this.ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    this.state = {
+      dataSource: this.ds.cloneWithRows(this.props.polls)
+    }
+  }
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.polls !== this.props.polls) {
+      this.setState({
+        dataSource: this.ds.cloneWithRows(nextProps.polls)
+      })
+    }
   }
   handleNewPoll = () => {
     this.props.navigator.push({
       newPoll: true
     })
   }
+  handlePollPress = (poll) => {
+    this.props.navigator.push({
+      poll: true,
+      id: poll.id,
+    })
+  }
+  renderRow = (data) => {
+    return (
+      <PollPreview
+        key={data.id}
+        onPress={() => this.handlePollPress(data)}
+        isOwnPoll={typeof this.props.ownPolls[data.id] !== 'undefined'}
+        hasTaken={typeof this.props.pollsVotedOn[data.id] !== 'undefined'}
+        data={data}/>
+    )
+  }
   render () {
     return (
       <Home
+        noPolls={Object.keys(this.props.polls).length === 0}
+        listenerSet={this.props.listenerSet}
+        dataSource={this.state.dataSource}
+        renderRow={this.renderRow}
         onNewPoll={this.handleNewPoll}
         openDrawer={this.props.openDrawer} />
     )
   }
 }
+
+function mapStateToProps ({polls, authentication}) {
+  return {
+    polls: polls.polls,
+    ownPolls: authentication.ownPolls,
+    pollsVotedOn: authentication.pollsVotedOn,
+    listenerSet: polls.listenerSet,
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(HomeContainer)

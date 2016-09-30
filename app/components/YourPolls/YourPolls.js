e3e037782a0189d6b6c852c32fb68dfe369a11ae
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-29 21:49:27 -0600

    Changes for YourPolls

--- app/components/YourPolls/YourPolls.js
+++ app/components/YourPolls/YourPolls.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react'
-import { View, StyleSheet, Text, Platform } from 'react-native'
+import { View, StyleSheet, Text, Platform, ScrollView } from 'react-native'
 import { PollsNavbar, HamburgerIcon, PollPreview } from '~/components'
+import { colors, fontSizes } from '~/styles'
 
 YourPolls.propTypes = {
+  polls: PropTypes.array.isRequired,
   openDrawer: PropTypes.func,
+  onPollPress: PropTypes.func.isRequired,
 }
 
 export default function YourPolls (props) {
@@ -12,9 +15,17 @@ export default function YourPolls (props) {
       <PollsNavbar
         leftButton={Platform.OS === 'android' ? <HamburgerIcon onPress={props.openDrawer} /> : null}
         title='Your Polls'/>
-      <Text>
-        YourPolls
-      </Text>
+      {props.polls.length === 0
+        ? <Text style={styles.noData}>Make your first poll! 🚀</Text>
+        : <ScrollView automaticallyAdjustContentInsets={false}>
+            {props.polls.map((data) => (
+              <PollPreview
+                isOwnPoll={true}
+                key={data.id}
+                onPress={() => props.onPollPress(data)}
+                data={data}/>
+            ))}
+          </ScrollView>}
     </View>
   )
 }
@@ -23,4 +34,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  noData: {
+    margin: 20,
+    textAlign: 'center',
+    fontSize: fontSizes.primary,
+    color: colors.primary,
+  }
 })
--- app/containers/YourPolls/YourPollsContainer.js
+++ app/containers/YourPolls/YourPollsContainer.js
@@ -1,15 +1,35 @@
 import React, { PropTypes, Component } from 'react'
+import { connect } from 'react-redux'
 import { YourPolls } from '~/components'
 
-export default class YourPollsContainer extends Component {
+class YourPollsContainer extends Component {
   static propTypes = {
+    polls: PropTypes.array.isRequired,
     openDrawer: PropTypes.func,
     navigator: PropTypes.object.isRequired,
+  }
+  handlePollPress = (poll) => {
+    this.props.navigator.push({
+      poll: true,
+      id: poll.id,
+    })
   }
   render () {
     return (
       <YourPolls
+        onPollPress={this.handlePollPress}
+        polls={this.props.polls}
         openDrawer={this.props.openDrawer} />
     )
   }
 }
+
+function mapStateToProps ({polls, authentication}) {
+  return {
+    polls: Object.keys(authentication.ownPolls).map((id) => polls.polls[id])
+  }
+}
+
+export default connect(
+  mapStateToProps
+)(YourPollsContainer)

8707da488a4b3400f751557c35b32eef727b16b5
Tyler McGinnis <tylermcginnis33@gmail.com>
2016-09-29 21:25:13 -0600

    Files for Devtools

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import { AppContainer } from '~/containers'
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
+import devTools from 'remote-redux-devtools'
 import * as reducers from './redux'
 
 const store = createStore(
   combineReducers(reducers),
-  applyMiddleware(thunk)
+  compose(
+    applyMiddleware(thunk),
+    devTools()
+  )
 )
 
 export default function Polls (props) {
